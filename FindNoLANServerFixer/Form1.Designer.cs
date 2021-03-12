c9ae3a639ae3261aba7c57d05c9cc4217275f92d
Michael Engelskirchen <michael.eng@hotmail.de>
2021-03-12 12:39:03 +0100

    Added Bin

--- FindNoLANServerFixer/Form1.Designer.cs
+++ FindNoLANServerFixer/Form1.Designer.cs
@@ -23,13 +23,77 @@
         /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbx_interfaces = new System.Windows.Forms.ComboBox();
+            this.lbl_interface = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(339, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mach den Rest für mich";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbx_interfaces
+            // 
+            this.cbx_interfaces.FormattingEnabled = true;
+            this.cbx_interfaces.Location = new System.Drawing.Point(15, 30);
+            this.cbx_interfaces.Name = "cbx_interfaces";
+            this.cbx_interfaces.Size = new System.Drawing.Size(257, 21);
+            this.cbx_interfaces.TabIndex = 1;
+            // 
+            // lbl_interface
+            // 
+            this.lbl_interface.AutoSize = true;
+            this.lbl_interface.Location = new System.Drawing.Point(12, 14);
+            this.lbl_interface.Name = "lbl_interface";
+            this.lbl_interface.Size = new System.Drawing.Size(342, 13);
+            this.lbl_interface.TabIndex = 2;
+            this.lbl_interface.Text = "Wähle das Netzwerkinterface über dem du den Server finden möchtest";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(282, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Zeige alle";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(365, 89);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lbl_interface);
+            this.Controls.Add(this.cbx_interfaces);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ich finde den Game Server nicht";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbx_interfaces;
+        private System.Windows.Forms.Label lbl_interface;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- FindNoLANServerFixer/Form1.cs
+++ FindNoLANServerFixer/Form1.cs
@@ -1,17 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace FindNoLANServerFixer {
+
     public partial class Form1 : Form {
+
         public Form1() {
             InitializeComponent();
+            cbx_interfaces.DisplayMember = "Description";
+            fetchNetworkInterfaces();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            NetworkInterface ni = (NetworkInterface)cbx_interfaces.SelectedItem;
+            int interfaceIndex = ni.GetIPProperties().GetIPv4Properties().Index;
+            String cmd1 = " /C route -4 ADD 255.255.255.255 MASK 255.255.255.255 0.0.0.0 METRIC 20 IF " + interfaceIndex;
+            String cmd2 = " /C route -4 CHANGE 255.255.255.255 MASK 255.255.255.255 0.0.0.0 METRIC 20 IF " + interfaceIndex;
+            Console.WriteLine(cmd1);
+            Process p1 = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo();
+            //psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = true;
+            psi.Verb = "runas";
+            psi.FileName = "cmd.exe";
+            psi.Arguments = @cmd1;
+            p1.StartInfo = psi;
+            p1.Start();
+            p1.Close();
+            psi.Arguments = @cmd2;
+            p1.Start();
+            p1.Close();
+        }
+
+        private void fetchNetworkInterfaces() {
+            cbx_interfaces.Items.Clear();
+            int lastIndex = 0;
+            NetworkInterface selectThis = null;
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces()) {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet ||
+                    ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+                    ni.NetworkInterfaceType == NetworkInterfaceType.FastEthernetT ||
+                    checkBox1.Checked) {
+                    cbx_interfaces.Items.Add(ni);
+                    if (lastIndex < ni.GetIPProperties().GetIPv4Properties().Index) {
+                        selectThis = ni;
+                        lastIndex = ni.GetIPProperties().GetIPv4Properties().Index;
+                    }
+                }
+            }
+            if (selectThis != null) {
+                cbx_interfaces.SelectedItem = selectThis;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            fetchNetworkInterfaces();
         }
     }
 }
